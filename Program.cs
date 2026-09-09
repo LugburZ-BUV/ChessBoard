99a587700f0a53c56a607b119778a8d8ce3b23ab
Luburz-BUV <Nils-olmas@live.se>
2026-09-09 12:59:18 +0200

    Schackbrädet med variabel för svarta/vita rutor Att göra: Placera ut pjäs på valfri ruta

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿namespace ChessBoard;
+//◼︎
+//◻︎
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Multiplikationstabell 1-5:");
+        Console.Write("Ange storleken på schakbrädet: ");
+        int width = int.Parse(Console.ReadLine());
+        Console.WriteLine("Ange den första symbolen för schakbrädet: ");
+        string white = Console.ReadLine();
+        Console.WriteLine("Ange den andra symbolen för schackbrädet: ");
+        string black = Console.ReadLine();
+        Console.WriteLine();
+        
+        for (int row = 1; row <= width; row++)
+        {
+            for (int column = 1; column <= width; column++)
+                if ((row + column) %2 == 0)
+                {
+                    Console.Write(white);
+                }
+                else
+                {
+                    Console.Write(black);
+                }
+            Console.WriteLine();
+        }
+            
+    }
+}
